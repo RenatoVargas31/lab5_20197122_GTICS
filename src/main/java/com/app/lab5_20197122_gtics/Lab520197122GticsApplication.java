36f13484424662313926060e575321d399537cc8
RenatoVargas31 <rlvs_312001_1999@outlook.com>
2024-09-25 19:27:10 -0500

    Initial commit

---
+++ src/main/java/com/app/lab5_20197122_gtics/Lab520197122GticsApplication.java
@@ -0,0 +1,13 @@
+package com.app.lab5_20197122_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab520197122GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab520197122GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/lab5_20197122_gtics/Lab520197122GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.lab5_20197122_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab520197122GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
